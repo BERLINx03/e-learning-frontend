114f492a0d37a08fe53b50ea356edc56381943de
abdallah <abdallahelsokkary4399@gmail.com>
2025-04-13 14:53:10 +0200

    seperate the thumbnail

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-    <Toaster 
+    <Toaster
       position="top-right"
       toastOptions={{
         duration: 3000,
